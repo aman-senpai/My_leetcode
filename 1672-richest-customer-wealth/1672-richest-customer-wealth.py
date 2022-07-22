88cbd34ce6839b54ba53158db0d1466fda3f8bd3
aman senpai <jemscollin1@gmail.com>
2022-07-23 01:24:51 +0530

    Time: 76 ms (62.19%), Space: 13.9 MB (73.50%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -2,9 +2,16 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         
         max_wealth_so_far = 0
+        
+        def sum_help(l1):
+            _sum = 0
+            for l in l1:
+                _sum += l
+            return _sum
+        
         for account in accounts:
             
-            curr_customer_wealth = sum(account)
+            curr_customer_wealth = sum_help(account)
             
             max_wealth_so_far = max(curr_customer_wealth, max_wealth_so_far)
             
