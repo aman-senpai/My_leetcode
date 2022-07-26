962dd8878236d7bd59919e9a57c466688d18ea88
aman senpai <jemscollin1@gmail.com>
2022-07-26 18:46:34 +0530

    Time: 46 ms (94.02%), Space: 13.9 MB (64.42%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -1,14 +1,20 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        ans = 0
-        n = len(arr)
-        presum = [0] * (n + 1)
         
+        res = 0; freq = 0; n = len(arr)
         for i in range(n):
-            presum[i + 1] += presum[i] + arr[i]
+            freq = freq-(i+1)//2+(n-i+1)//2
+            res += freq*arr[i]
+        return res
+#         ans = 0
+#         n = len(arr)
+#         presum = [0] * (n + 1)
         
-        for i in range(n): 
-            for j in range(i+1, n+1):
-                if (j - i) % 2:
-                    ans += presum[j] - presum[i]
-        return ans
+#         for i in range(n):
+#             presum[i + 1] += presum[i] + arr[i]
+        
+#         for i in range(n): 
+#             for j in range(i+1, n+1):
+#                 if (j - i) % 2:
+#                     ans += presum[j] - presum[i]
+#         return ans
