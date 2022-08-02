0db0c5ad34654091f8f11fee0f87819c28a219ba
aman senpai <jemscollin1@gmail.com>
2022-08-02 09:00:42 +0530

    Time: 196 ms (64.13%), Space: 16.7 MB (84.56%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -3,5 +3,6 @@ class Solution:
         res = 0
         
         for n in nums:
-            res = n ^ res
+            res ^= n # res is XORed with n
+                     # XOR done with same number gives all zeros
         return res
