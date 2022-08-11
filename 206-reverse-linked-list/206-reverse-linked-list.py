bf4d9d7430f56cc8724bd6022bb0778be96e21db
aman senpai <jemscollin1@gmail.com>
2022-08-11 21:56:56 +0530

    Time: 81 ms (7.11%), Space: 15.4 MB (94.21%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -8,23 +8,23 @@ class Solution:
 
 # Recursive
 
-        if not head:
-            return None
+#         if not head:
+#             return None
         
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
+#         newHead = head
+#         if head.next:
+#             newHead = self.reverseList(head.next)
+#             head.next.next = head
+#         head.next = None
         
-        return newHead
+#         return newHead
         
 # Iterative
-#         prev, curr = None, head
+        prev, curr = None, head
         
-#         while curr:
-#             nxt = curr.next
-#             curr.next = prev
-#             prev = curr
-#             curr = nxt
-#         return prev
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
