b95cb3d7154d9bd75494a0da11e8db9467c097dd
aman senpai <jemscollin1@gmail.com>
2022-08-13 23:13:16 +0530

    Time: 361 ms (45.62%), Space: 15.6 MB (84.74%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.py
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -1,15 +1,11 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        circle = [x for x in nums]
-        for n in nums:
-            circle.append(n)
-        
-        res = []
-        for i, n in enumerate(nums):
-            for j in range(i, len(circle)):
-                if circle[j] > n:
-                    res.append(circle[j])
-                    break
-            else:
-                res.append(-1)
+        stack, res = [], [-1] * len(nums)
+        for i, num in enumerate(nums):
+            while stack and nums[stack[-1]] < num:
+                res[stack.pop()] = num
+            stack.append(i)
+        for i, num in enumerate(nums):
+            while stack and nums[stack[-1]] < num:
+                res[stack.pop()] = num
         return res
