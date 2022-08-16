8e03549afd0fb9b535135b2106250eb8071191c1
aman senpai <jemscollin1@gmail.com>
2022-08-16 12:02:17 +0530

    Time: 121 ms (33.59%), Space: 13.9 MB (44.03%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -9,7 +9,6 @@ class Solution:
             curr = dummy
             
             carry = 0
-            
             while l1 or l2 or carry:
                 v1 = l1.val if l1 else 0
                 v2 = l2.val if l2 else 0
